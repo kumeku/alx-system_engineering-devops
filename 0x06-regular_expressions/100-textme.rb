8f7404eab065d5afc937cba5a1cb1c9739726996
kumeku <101403261+kumeku@users.noreply.github.com>
2022-08-23 19:06:47 +0100

    regular expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+print ARGV[0].scan(/(?<=from:)[+0-9a-zA-Z]+/).join
+print ","
+print ARGV[0].scan(/(?<=to:)[+0-9a-zA-Z]+/).join
+print ","
+puts ARGV[0].scan(/(?<=flags:)[-:0-9]+/).join
